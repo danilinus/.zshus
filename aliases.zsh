cd348e22516464e5336fa00d5d005fd77278dbf9
danilinus <danilagalkevich@gmail.com>
2026-05-19 09:58:22 +0300

    feat: aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,16 @@
-alias zshconfig="nano ~/.zshrc"
+alias zshconfig='$EDITOR $HOME/.zshrc'
 alias ll="ls -l"
 alias la="ls -al"
 alias ..="cd .."
 alias ...="cd ../.."
+
+# Обновление zshus конфига
+alias zshus='bash "$HOME/.zshus/install.sh"'
+# Вариации
+alias zup='zshus'
+alias us='zshus'
+
+alias usconfig='$EDITOR "$HOME/.zshus/zshus.zsh"'
+alias usexport='$EDITOR "$HOME/.zshus/exports.zsh"'
+alias usalias='$EDITOR "$HOME/.zshus/aliases.zsh"'
+alias usbind='$EDITOR "$HOME/.zshus/bindings.zsh"'
